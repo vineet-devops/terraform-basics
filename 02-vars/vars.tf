96b30afd47fd05fb01da7d30c6d53670412f7adb
vineet <vineetsingh49@gmail.com>
2023-07-16 15:44:28 +0530

    Auto Push

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -2,11 +2,24 @@ variable "sample" {
     default = "Welcome to terraform"
 }
 
-output "sample-op"{
+output "sample_op"{
     value = var.sample
 }
 
-output "var-sample-op"{
-    value = "value of the variable is ${var.sample-op}"
+output "var_sample_op"{
+    value = "value of the variable is ${var.sample_op}"
 }
 
+variable "example_list"{
+    default = {
+        "aws",
+        "DevOps",
+        54,
+        "Trainer",
+        "terraform"
+    }
+}
+
+output "example_list_op" {
+    value = "welcome to ${var.example_list[1]} with ${var.example_list[0]} trainning and this is our batch ${var.example_list[2]} and current topic is ${var.example_list[4]}"
+}

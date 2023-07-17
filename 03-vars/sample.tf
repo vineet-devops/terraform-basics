e473ab70a5c64d4698d7a4286aa5e5cd6a5a4f8e
vineet <vineetsingh49@gmail.com>
2023-07-17 08:58:59 +0530

    Auto Push

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -1,7 +1,17 @@
-variable "city"{
-
-}
+variable "city"{}
 
 output "city_name" {
     value = "our city name is ${var.city}"
+}
+
+variable "state"{}
+
+output "state_name"{
+    value = "Our State name is ${var.state}"
+}
+
+variable "cold_city"{}
+
+output "cold_city_output"{
+    value = "Cold city name is ${var.cold_city}"
 }
